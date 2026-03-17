0b6f73a92e346a2b8b7b7a19fde7e902d318819d
Mehmt-Emin <mhmt0emn@gmail.com>
2026-03-18 01:01:31 +0300

    ToDo uygulaması geliştirildi - görev düzenleme eklendi - tamamlanan görevler gizleme özelliği - görev sayacı eklendi - tasarım iyileştirildi

--- ToDoDesktop/ToDoForm.Designer.cs
+++ ToDoDesktop/ToDoForm.Designer.cs
@@ -37,116 +37,184 @@
             this.btnTamamlandi = new System.Windows.Forms.Button();
             this.btnGeri = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chkTamamlananlariGizle = new System.Windows.Forms.CheckBox();
+            this.lblSayac = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblGorev
             // 
             this.lblGorev.AutoSize = true;
             this.lblGorev.BackColor = System.Drawing.Color.Transparent;
-            this.lblGorev.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblGorev.ForeColor = System.Drawing.Color.LightCoral;
-            this.lblGorev.Location = new System.Drawing.Point(204, 54);
+            this.lblGorev.Font = new System.Drawing.Font("Segoe UI", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGorev.ForeColor = System.Drawing.Color.DimGray;
+            this.lblGorev.Location = new System.Drawing.Point(185, 54);
             this.lblGorev.Name = "lblGorev";
-            this.lblGorev.Size = new System.Drawing.Size(128, 25);
+            this.lblGorev.Size = new System.Drawing.Size(138, 30);
             this.lblGorev.TabIndex = 0;
             this.lblGorev.Text = "Görev Girin:";
+            this.lblGorev.Click += new System.EventHandler(this.lblGorev_Click);
             // 
             // txtGorev
             // 
-            this.txtGorev.BackColor = System.Drawing.Color.Honeydew;
-            this.txtGorev.Location = new System.Drawing.Point(366, 55);
+            this.txtGorev.BackColor = System.Drawing.Color.White;
+            this.txtGorev.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGorev.Location = new System.Drawing.Point(376, 54);
             this.txtGorev.Name = "txtGorev";
             this.txtGorev.Size = new System.Drawing.Size(246, 26);
             this.txtGorev.TabIndex = 1;
+            this.txtGorev.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtGorev_KeyDown);
             // 
             // lstGorevler
             // 
+            this.lstGorevler.BackColor = System.Drawing.Color.White;
             this.lstGorevler.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lstGorevler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstGorevler.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstGorevler.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lstGorevler.ForeColor = System.Drawing.Color.LightCoral;
             this.lstGorevler.FormattingEnabled = true;
-            this.lstGorevler.ItemHeight = 22;
-            this.lstGorevler.Location = new System.Drawing.Point(2, -3);
+            this.lstGorevler.ItemHeight = 30;
+            this.lstGorevler.Location = new System.Drawing.Point(0, 0);
             this.lstGorevler.Name = "lstGorevler";
-            this.lstGorevler.Size = new System.Drawing.Size(376, 286);
+            this.lstGorevler.Size = new System.Drawing.Size(376, 334);
             this.lstGorevler.TabIndex = 2;
+            this.lstGorevler.SelectedIndexChanged += new System.EventHandler(this.lstGorevler_SelectedIndexChanged);
+            this.lstGorevler.DoubleClick += new System.EventHandler(this.lstGorevler_DoubleClick);
             // 
             // btnEkle
             // 
+            this.btnEkle.BackColor = System.Drawing.Color.White;
+            this.btnEkle.FlatAppearance.BorderSize = 0;
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnEkle.ForeColor = System.Drawing.Color.DarkTurquoise;
-            this.btnEkle.Location = new System.Drawing.Point(668, 55);
+            this.btnEkle.Location = new System.Drawing.Point(700, 54);
             this.btnEkle.Name = "btnEkle";
             this.btnEkle.Size = new System.Drawing.Size(164, 34);
             this.btnEkle.TabIndex = 3;
             this.btnEkle.Text = "Ekle";
-            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.UseVisualStyleBackColor = false;
             this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
             // btnSil
             // 
+            this.btnSil.BackColor = System.Drawing.Color.White;
+            this.btnSil.FlatAppearance.BorderSize = 0;
+            this.btnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnSil.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnSil.Location = new System.Drawing.Point(238, 509);
+            this.btnSil.Location = new System.Drawing.Point(159, 533);
             this.btnSil.Name = "btnSil";
             this.btnSil.Size = new System.Drawing.Size(164, 34);
             this.btnSil.TabIndex = 4;
             this.btnSil.Text = "Sil";
-            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.UseVisualStyleBackColor = false;
             this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
             // btnTamamlandi
             // 
+            this.btnTamamlandi.BackColor = System.Drawing.Color.White;
+            this.btnTamamlandi.FlatAppearance.BorderSize = 0;
+            this.btnTamamlandi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTamamlandi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnTamamlandi.ForeColor = System.Drawing.Color.ForestGreen;
-            this.btnTamamlandi.Location = new System.Drawing.Point(505, 509);
+            this.btnTamamlandi.Location = new System.Drawing.Point(410, 533);
             this.btnTamamlandi.Name = "btnTamamlandi";
             this.btnTamamlandi.Size = new System.Drawing.Size(164, 34);
             this.btnTamamlandi.TabIndex = 5;
             this.btnTamamlandi.Text = "Tamamlandı";
-            this.btnTamamlandi.UseVisualStyleBackColor = true;
+            this.btnTamamlandi.UseVisualStyleBackColor = false;
             this.btnTamamlandi.Click += new System.EventHandler(this.btnTamamlandi_Click);
             // 
             // btnGeri
             // 
+            this.btnGeri.BackColor = System.Drawing.Color.White;
+            this.btnGeri.FlatAppearance.BorderSize = 0;
+            this.btnGeri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGeri.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnGeri.Location = new System.Drawing.Point(761, 509);
+            this.btnGeri.Location = new System.Drawing.Point(657, 533);
             this.btnGeri.Name = "btnGeri";
             this.btnGeri.Size = new System.Drawing.Size(164, 34);
             this.btnGeri.TabIndex = 6;
             this.btnGeri.Text = "Ana Menüye Dön";
-            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.UseVisualStyleBackColor = false;
             this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
             // 
             // panel1
             // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.lstGorevler);
-            this.panel1.Location = new System.Drawing.Point(305, 166);
+            this.panel1.Location = new System.Drawing.Point(306, 164);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(378, 286);
+            this.panel1.Size = new System.Drawing.Size(376, 334);
             this.panel1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(937, 624);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chkTamamlananlariGizle
+            // 
+            this.chkTamamlananlariGizle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.chkTamamlananlariGizle.BackColor = System.Drawing.Color.White;
+            this.chkTamamlananlariGizle.FlatAppearance.BorderSize = 0;
+            this.chkTamamlananlariGizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkTamamlananlariGizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkTamamlananlariGizle.Location = new System.Drawing.Point(411, 573);
+            this.chkTamamlananlariGizle.Name = "chkTamamlananlariGizle";
+            this.chkTamamlananlariGizle.Size = new System.Drawing.Size(224, 34);
+            this.chkTamamlananlariGizle.TabIndex = 9;
+            this.chkTamamlananlariGizle.Text = "Tamamlananları Gizle";
+            this.chkTamamlananlariGizle.UseVisualStyleBackColor = false;
+            this.chkTamamlananlariGizle.CheckedChanged += new System.EventHandler(this.chkTamamlananlariGizle_CheckedChanged);
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.BackColor = System.Drawing.Color.Transparent;
+            this.lblSayac.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.ForeColor = System.Drawing.Color.DimGray;
+            this.lblSayac.Location = new System.Drawing.Point(186, 99);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(161, 30);
+            this.lblSayac.TabIndex = 10;
+            this.lblSayac.Text = "Görev Sayısı : 0";
             // 
             // ToDoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(937, 568);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(937, 624);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.chkTamamlananlariGizle);
+            this.Controls.Add(this.lblGorev);
+            this.Controls.Add(this.txtGorev);
+            this.Controls.Add(this.btnEkle);
             this.Controls.Add(this.btnGeri);
             this.Controls.Add(this.btnTamamlandi);
             this.Controls.Add(this.btnSil);
-            this.Controls.Add(this.btnEkle);
-            this.Controls.Add(this.txtGorev);
-            this.Controls.Add(this.lblGorev);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.Name = "ToDoForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Görevler";
             this.Load += new System.EventHandler(this.ToDoForm_Load);
+            this.Resize += new System.EventHandler(this.ToDoForm_Resize);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,5 +230,8 @@
         private System.Windows.Forms.Button btnTamamlandi;
         private System.Windows.Forms.Button btnGeri;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chkTamamlananlariGizle;
+        private System.Windows.Forms.Label lblSayac;
     }
 }
--- ToDoDesktop/ToDoForm.cs
+++ ToDoDesktop/ToDoForm.cs
@@ -7,20 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
 
 namespace ToDoDesktop
 {
     public partial class ToDoForm : Form
     {
+        string dosyaYolu = "gorevler.txt";
+        List<string> tumGorevler = new List<string>();
         public ToDoForm()
         {
             InitializeComponent();
         }
 
-       
+        private void ListeyiGuncelle()
+        {
+            lstGorevler.Items.Clear();
+
+            foreach (string gorev in tumGorevler)
+            {
+                if (chkTamamlananlariGizle.Checked && gorev.StartsWith("✔ "))
+                {
+                    continue;
+                }
+                lstGorevler.Items.Add(gorev);
+            }
+            lblSayac.Text = "Görev Sayısı: " + tumGorevler.Count;
+        }
+        
+        private void GorevleriKaydet()
+        {
+            
+            File.WriteAllLines(dosyaYolu, tumGorevler);
+        }
+
+        private void GorevleriYukle()
+        {
+            if (File.Exists(dosyaYolu))
+            {
+               tumGorevler = File.ReadAllLines(dosyaYolu).ToList();
+            }
+                ListeyiGuncelle();
+        }
 
         private void ToDoForm_Load(object sender, EventArgs e)
         {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.MaximizeBox = true;
+
+            GorevleriYukle();
+
+            pictureBox1.SendToBack();
+            panel1.BringToFront();
 
         }
 
@@ -28,9 +67,11 @@ namespace ToDoDesktop
         {
             if (!string.IsNullOrWhiteSpace(txtGorev.Text))
             {
-                lstGorevler.Items.Add(txtGorev.Text);
+                tumGorevler.Add(txtGorev.Text);
                 txtGorev.Clear();
                 txtGorev.Focus();
+                GorevleriKaydet();
+                ListeyiGuncelle();
             }
             else
             {
@@ -42,7 +83,15 @@ namespace ToDoDesktop
         {
             if (lstGorevler.SelectedItem != null)
             {
-                lstGorevler.Items.Remove(lstGorevler.SelectedItem);
+                DialogResult sonuc = MessageBox.Show("Bu görevi silmek istediğinize emin misiniz?", "Görev Sil", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (sonuc == DialogResult.Yes)
+                {
+                    string seciliGorev = lstGorevler.SelectedItem.ToString();
+                    tumGorevler.Remove(seciliGorev);
+
+                    GorevleriKaydet();
+                    ListeyiGuncelle();
+                }
             }
             else
             {
@@ -54,16 +103,22 @@ namespace ToDoDesktop
         {
             if (lstGorevler.SelectedItem != null)
             {
-                int seciliIndex = lstGorevler.SelectedIndex;
-                string gorev = lstGorevler.SelectedItem.ToString();
+                string seciliGorev = lstGorevler.SelectedItem.ToString();
 
-                if (!gorev.StartsWith("✔ "))
+                if (!seciliGorev.StartsWith("✔ "))
                 {
-                    lstGorevler.Items[seciliIndex] = "✔ " + gorev;
+                    int index = tumGorevler.IndexOf(seciliGorev);
+
+                    if (index != -1)
+                    {
+                        tumGorevler[index] = "✔ " + seciliGorev;
+                        GorevleriKaydet();
+                        ListeyiGuncelle();
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Bu görev zaten tamamlandı olarak işaretlenmiş.");
+                    MessageBox.Show("Bu görev zaten tamamlandı.");
                 }
             }
             else
@@ -71,12 +126,124 @@ namespace ToDoDesktop
                 MessageBox.Show("Lütfen tamamlandı yapmak için bir görev seçin.");
             }
         }
-       
+
         private void btnGeri_Click(object sender, EventArgs e)
         {
             Form1 anaMenu = new Form1();
             anaMenu.Show();
             this.Hide();
+        }
+
+        private void lstGorevler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtGorev_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEkle.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void lstGorevler_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstGorevler.SelectedItem != null)
+            {
+                string eskiGorev = lstGorevler.SelectedItem.ToString();
+                int index = tumGorevler.IndexOf(eskiGorev);
+
+                if (index == -1) return;
+
+                bool tamamlandiMi = eskiGorev.StartsWith("✔ ");
+                string duzenlenecekMetin = eskiGorev;
+
+                if (tamamlandiMi)
+                {
+                    duzenlenecekMetin = eskiGorev.Substring(2);
+                }
+
+                string yeniGorev = Microsoft.VisualBasic.Interaction.InputBox(
+                    "Görevi düzenleyin:",
+                    "Görev Düzenle",
+                    duzenlenecekMetin
+                );
+
+                if (!string.IsNullOrWhiteSpace(yeniGorev))
+                {
+                    if (tamamlandiMi)
+                        tumGorevler[index] = "✔ " + yeniGorev;
+                    else
+                        tumGorevler[index] = yeniGorev;
+
+                    GorevleriKaydet();
+                    ListeyiGuncelle();
+                }
+            }
+        }
+
+        private void ToDoForm_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void KontrolleriYerlestir()
+        {
+            int pw = pictureBox1.Width;
+            int ph = pictureBox1.Height;
+
+            int px = pictureBox1.Left;
+            int py = pictureBox1.Top;
+
+            panel1.Left = px + (int)(pw * 0.39);
+            panel1.Top = py + (int)(ph * 0.24);
+            panel1.Width = (int)(pw * 0.40);
+            panel1.Height = (int)(ph * 0.54);
+
+            lblGorev.Left = px + (int)(pw * 0.20);
+            lblGorev.Top = py + (int)(ph * 0.10);
+
+            txtGorev.Left = px + (int)(pw * 0.40);
+            txtGorev.Top = py + (int)(ph * 0.09);
+            txtGorev.Width = (int)(pw * 0.18);
+
+            btnEkle.Left = px + (int)(pw * 0.65);
+            btnEkle.Top = py + (int)(ph * 0.09);
+            btnEkle.Width = (int)(pw * 0.16);
+
+            btnSil.Left = px + (int)(pw * 0.20);
+            btnSil.Top = py + (int)(ph * 0.88);
+            btnSil.Width = (int)(pw * 0.18);
+
+            btnTamamlandi.Left = px + (int)(pw * 0.48);
+            btnTamamlandi.Top = py + (int)(ph * 0.88);
+            btnTamamlandi.Width = (int)(pw * 0.18);
+
+            btnGeri.Left = px + (int)(pw * 0.73);
+            btnGeri.Top = py + (int)(ph * 0.88);
+            btnGeri.Width = (int)(pw * 0.20);
+
+            chkTamamlananlariGizle.Left = pictureBox1.Left + (int)(pictureBox1.Width * 0.03);
+            chkTamamlananlariGizle.Top = pictureBox1.Top + (int)(pictureBox1.Height * 0.88);
+            chkTamamlananlariGizle.Width = (int)(pictureBox1.Width * 0.22);
+            chkTamamlananlariGizle.Height = 30;
+
+            lblSayac.Left = px + (int)(pw * 0.03);
+            lblSayac.Top = py + (int)(ph * 0.83);
+            lblSayac.Width = (int)(pw * 0.20);
+            lblSayac.Height = 30;
+
+        }
+
+        private void lblGorev_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkTamamlananlariGizle_CheckedChanged(object sender, EventArgs e)
+        {
+            ListeyiGuncelle();
         }
     }
 }
